f19a8321f06ad622a3ed21e14a9520184f931ed4
Fisher Ramsey <fishram@unc.edu>
2024-05-15 21:21:37 -0400

    some simple js with my car

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+let car = {
+    brand: 'Toyota',
+    miles: 225000
+};
+console.log(car.brand);
+
+function add1kMiles() {
+    car.miles += 1000;
+}
+add1kMiles();
+console.log(car.miles);
