55a67d6a7ed727c995d241af450055dd1731c275
Fisher Ramsey <fishram@unc.edu>
2024-05-15 21:35:39 -0400

    max of 2 nums

--- code.js
+++ code.js
@@ -9,3 +9,11 @@ function add1kMiles() {
 }
 add1kMiles();
 console.log(car.miles);
+
+switch(car.miles){
+    case 226000:
+        console.log("Your Toyota has 226000 miles")
+        break;
+    default:
+        console.log("Defaul miles on your Toyota")
+}
